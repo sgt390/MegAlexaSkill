72adc75b221f35b7892f632a70eb38668dbdee85
sgt390 <stefano.sgt39@gmail.com>
2019-02-21 17:58:00 +0100

    0.1

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const Alexa = require('ask-sdk-core');
+const Alexa = require('alexa-sdk');
 exports.handler = (event, context, callback) => {
     const alexa = Alexa.handler(event,context);
-
     alexa.appId = process.env.APP_ID;
-    alexa.registerHandlers(newSessionHandler,startWorkflowHandlers);
+    alexa.registerHandlers(newSessionHandler,startWorkflowHandlers, outOfSessionHandler,pinDemoHandlers,boxDemoHandlers);
     alexa.execute();
-/*    switch (event.request.type) {
-case "LaunchRequest":
-    context.succeed(generateResponse(buildSpeechletResponse("Welcome to megalexa.", false)));
-    break;
-case "IntentRequest":
-    switch (event.request.intent.name) {
-    case "workflow":
-    context.succeed(generateResponse(buildSpeechletResponse("Workflow", true)));
-    break;
-    }
-    break;
-    }
-*/
+
 }
+
+const BUILTIN_BLOCK_STATE = Array.from(Array(3).keys()).map(k => "BUILTIN_BLOCK_STATE" + k)
 
 const newSessionHandler = {
     LaunchRequest() {
@@ -36,9 +25,94 @@ const startWorkflowHandlers = Alexa.CreateStateHandler("WORKFLOW_SELECTION_MODE"
         this.emit("ask", "response.say()"); 
     });
     */
-   // testing workflow without external functions
-    "BuiltInWorkflowIntent": function () {
+   ///////////// <testing workflow without external functions> demo ///////////////////////////////////
+    "WorkflowIntent": function () {
         this.handler.state = "BUILT_IN_WORKFLOW";
-        this.emit(":ask", "you selected the built in workflow");
+        var workflow_user = this.event.request.intent.slots.workflow_name.value;
+        this.speak("You joined "+ workflow_user +"! Say ok to continue");
+        /*else
+        {
+            this.speak("the workflow does not exist, please repeat")
+                .listen("please say a workflow name");
+        }*/
+        this.emit(":responseReady");
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        // #TODO save state
+        this.emit(':tell', 'bye');
+    },
+     
+    'Unhandled': function() {
+        this.response.speak('Sorry, I didn\'t get that. Try saying a workflow name.')
+                    .listen('Try saying your workflow name.');
+        this.emit(':responseReady');
+    }
+});
+
+
+const outOfSessionHandler = {
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        //this.emit(':saveState', true); // Be sure to call :saveState to persist your session attributes in DynamoDB
+        this.emit(':tell', 'bye');
+    },
+     
+    'Unhandled': function() {
+        this.response.speak('Please register into the megalexa app.');
+        this.emit(':responseReady');
+    }
+};
+
+//////////////////////////////*  demo   *///////////////////////////////
+const pinDemoHandlers = Alexa.CreateStateHandler(BUILTIN_BLOCK_STATE[0], {
+
+    "pinDemoIntent": function () {
+        if(this.event.request.intent.slots.pin.value)
+        {
+        this.speak("you are inside the pin block. say 4 numbers")
+            .response("4 numbers!");
+        }else
+        {
+            this.handler.state = BUILTIN_BLOCK_STATE[1];
+            this.speak("the pin was correct! say go to continue");
+        }
+        this.emit(":responseReady");
+
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.emit(':tell', 'you left the demo workflow in state:' + BUILTIN_BLOCK_STATE[0]);
+    },
+     
+    'Unhandled': function() {
+        this.response.speak('error in demo workflow: ' +BUILTIN_BLOCK_STATE[0])
+                    .listen('error in demo workflow');
+        this.emit(':responseReady');
+    }
+});
+const boxDemoHandlers = Alexa.CreateStateHandler(BUILTIN_BLOCK_STATE[1], {
+
+    "boxDemoIntent": function () {
+        this.handler.state = "WORKFLOW_SELECTION_MODE";
+        this.emit(":ask", "you are inside the text-box block. say go to continue");
+    },
+
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.attributes['endedSessionCount'] += 1;
+        // #TODO save state
+        this.emit(':tell', 'you left the demo workflow in state:' + BUILTIN_BLOCK_STATE[1]);
+    },
+     
+    'Unhandled': function() {
+        this.response.speak('error in demo workflow: ' +BUILTIN_BLOCK_STATE[1])
+                    .listen('error in demo workflow');
+        this.emit(':responseReady');
     }
 });
