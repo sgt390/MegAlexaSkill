54dfed7cccc09d8f5df37978f9a13887a8920a08
sgt390 <stefano.sgt39@gmail.com>
2019-02-22 18:17:25 +0100

    npm publish command

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MegAlexaSkill
+
+## npm scripts
+### npm run test
+\#todo
+### npm run publish
+push nodejs code to AWS Lambda.
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,4 @@
-bash build.sh
+cd scripts
+sh build.sh
 aws lambda update-function-code --function-name megalexa --zip-file fileb://lambda.zip
+rm lambda.zip
