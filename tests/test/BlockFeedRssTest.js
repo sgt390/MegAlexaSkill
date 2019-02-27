3f090c61ab94e0ebf12f4afe38a9c1d91f7cfdfa
sgt390 <stefano.sgt39@gmail.com>
2019-02-27 11:58:46 +0100

    added tests headers

--- tests/test/BlockFeedRssTest.js
+++ tests/test/BlockFeedRssTest.js
@@ -1,13 +1,13 @@
 /*
-* File: Workflow.js
+* File: BlockFeedRssTest.js
 * Version: 0.0.1
-* Date: Date: 2019-02-25
+* Date: Date: 2019-02-26
 * Author: Stefano Zanatta
 * License:
 *
 * History:
 * Author            || Date         ||  descriptor
-* Stefano Zanatta   || 2019-02-25   || Created file
+* Stefano Zanatta   || 2019-02-26   || Created file
 */
 const assert = require('chai').assert;
 //const NameBlock = require(../../lambda/classname);
--- tests/test/DemoTest.js
+++ tests/test/DemoTest.js
@@ -1,3 +1,14 @@
+/*
+* File: DemoTest.js
+* Version: 0.0.1
+* Date: Date: 2019-02-26
+* Author: Stefano Zanatta
+* License:
+*
+* History:
+* Author            || Date         ||  descriptor
+* Stefano Zanatta   || 2019-02-26   || Created file
+*/
 var assert = require('chai').assert;
 //const myTestingBlock = require(..);
 
