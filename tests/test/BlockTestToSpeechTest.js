fa5ff2736733d4b820aa056df297e233ccc2373d
sgt390 <stefano.sgt39@gmail.com>
2019-02-27 11:39:42 +0100

    test

---
+++ tests/test/BlockTestToSpeechTest.js
@@ -0,0 +1,11 @@
+
+const assert = require('chai').assert();
+//const TTSBlock = require(../../scripts);
+
+describe('FunctionName', function(){
+    it('app should return hello', function(){
+        // let result = myfunctionName(..)
+        // assert.equal(result,...);
+        assert.equals(1,1);
+    });
+});
---
+++ tests/test/DemoTest.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert();
+//const myTestingBlock = require(..);
+
+describe('DemoTest', function(){
+    it('app should return hello', function(){
+        assert.equal('hel' + 'lo', 'hello');
+    });
+});
