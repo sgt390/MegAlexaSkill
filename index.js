b77866fe5a0da022ec05544262565ec7b66a1f34
sgt390 <stefano.sgt39@gmail.com>
2019-02-28 15:21:50 +0100

    better folders

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Alexa = require('ask-sdk');
+const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,14 @@
 cd scripts
 rm lambda.zip
 cp ../index.js ../lambda/
+rm -R ../lambda/node_modules
+mkdir ../lambda/node_modules
+mv ../node_modules/ask-sdk-core ../lambda/node_modules/ask-sdk-core
+mv ../node_modules/ask-sdk-model ../lambda/node_modules/ask-sdk-model
+mv ../node_modules/ask-sdk-runtime ../lambda/node_modules/ask-sdk-runtime
+mv ../lambda/node_modules/ask-sdk-core ../node_modules/ask-sdk-core
+mv ../lambda/node_modules/ask-sdk-model ../node_modules/ask-sdk-model
+mv ../lambda/node_modules/ask-sdk-runtime ../node_modules/ask-sdk-runtime
 7z a -tzip lambda.zip ../lambda/*
 rm ../lambda/index.js
+rm -R ../lambda/node_modules
