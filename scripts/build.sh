050d1ac47163132f7ed432a0b666d95ceb460b39
sgt390 <stefano.sgt39@gmail.com>
2019-03-04 16:43:50 +0100

    fixing scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,5 @@
 cd scripts
 rm lambda.zip
-cp ../index.js ../lambda/
-rm -R ../lambda/node_modules
-mv ../node_modules ../lambda/node_modules
+7z a -tzip lambda.zip ../index.js
+7z a -tzip lambda.zip ../node_modules
 7z a -tzip lambda.zip ../lambda/*
-rm ../lambda/index.js
-mv ../lambda/node_modules ../node_modules
