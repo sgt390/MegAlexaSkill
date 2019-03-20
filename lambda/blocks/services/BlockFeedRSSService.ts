7b3bac0406d0ffa3bb34935698624bfa10768a9e
sgt390 <stefano.sgt39@gmail.com>
2019-03-20 09:57:37 +0100

    services

---
+++ lambda/blocks/services/BlockFeedRSSService.ts
@@ -0,0 +1,17 @@
+import { BlockFeedRSS } from "../concreteBlocks/BlockFeedRSS";
+
+/*
+* File: Workflow.js
+* Version: 0.0.1
+* Date: 2019-02-25
+* Author: Matteo Depascale
+* License:
+*
+* History:
+* Author                || Date         || Description
+* Stefano Zanatta       || 2019-03-19   || Created file
+*/
+
+class BlockFeedRssService {
+
+}
