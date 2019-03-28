4fb474ff983b90d6a0e3f7dd21d502316b6fa392
sgt390 <stefano.sgt39@gmail.com>
2019-03-28 15:39:55 +0100

    +

--- lambda/connectors/ConnectorBlockTwitter.ts
+++ lambda/connectors/ConnectorBlockTwitter.ts
@@ -10,11 +10,10 @@
 * Matteo Depascale      || 2019-03-27   || Created file
 */
 import { ConnectorBlock } from "./ConnectorBlock";
-import { connectorTwitter } from "../JSONconfigurations/JSONconfiguration";
 const Twitter = require('twitter');
 
 //not best practice, twitter has credential of ZeroSeven, regenerete them at https://developer.twitter.com/en/apps/16179148
-const twitter = new Twitter({
+const user = new Twitter({
     consumer_key: 'omCc4IgufpfW8VUu3bq6OwPBo ',
     consumer_secret: 'Ji3e2BoAp4E58DZS8ycT0KoopG1ArPNtjoaZBxfi5jQoAEfhsb ',
     access_token_key: '1110935101561556992-eFkJvjMWNDn9UjiSCVX8IwvhM3qnp6 ',
@@ -35,14 +34,26 @@ const params = {
 }
 
 export class ConnectorBlockTwitter implements ConnectorBlock {
+
+
+    error = function (err: any) {
+        console.log('ERROR [%s]', err);
+        return err;
+	};
+	success = function (data:any) {
+        console.log('Data [%s]', data);
+        return data;
+	};
     
-    constructor(private params: string) {
-
-    }
-
     connect(limit:number = Number.POSITIVE_INFINITY): Promise<string> {
-        let tweets = twitter.getSearch('search/tweets', this.params, 
+        return user.get('search/tweets', function(error: any, tweets: any, response:any) {
+            //if(error) throw error;
+            console.log(tweets);  // The favorites.
+            //console.log(response);  // Raw response object.
+          });
     }
 
 }
 
+let c = new ConnectorBlockTwitter();
+c.connect(); //.then(a => console.log(a)).catch(a => console.log('123'));
