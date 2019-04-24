bf70680dc50e9804275e918bab795df2abdd9b23
sgt390 <stefano.sgt39@gmail.com>
2019-04-24 16:32:31 +0200

    1

---
+++ lambda/connectors/ConnectorBlockEmail.js
@@ -0,0 +1,116 @@
+"use strict";
+/*
+* File: ConnectorBlockEmail.ts
+* Version: 0.0.1
+* Date: 2019-03-20
+* Author: Matteo Depascale
+* License:
+*
+* History:
+* Author                || Date         || Description
+* Matteo Depascale      || 2019-03-20   || Created file
+*/
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var google = require('googleapis').google;
+//////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+var ConnectorBlockEmail = /** @class */ (function () {
+    /////////////////////// CREATE CREDENTIALS TYPE! ///////////////////////////
+    function ConnectorBlockEmail(token, credentials) {
+        this.limit = 10;
+        this.oAuth2Client = this.authorize(token, credentials);
+    }
+    ConnectorBlockEmail.prototype.authorize = function (token, credentials) {
+        var _a = credentials.installed, client_secret = _a.client_secret, client_id = _a.client_id, redirect_uris = _a.redirect_uris;
+        var oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+        return oAuth2Client.setCredentials(token);
+    };
+    ConnectorBlockEmail.prototype.connect = function () {
+        return this.listMessages(this.oAuth2Client, 'label:unread', function (el) { return console.log(el); });
+    };
+    ConnectorBlockEmail.prototype.listMessages = function (auth, query, callback) {
+        var gmail = google.gmail({ version: 'v1', auth: auth });
+        return gmail.users.messages.list({
+            'userId': 'me',
+            'q': query
+        }).then(function (res) {
+            return __awaiter(this, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, res.data.messages.reduce(function (buffer, messageInfo) {
+                                return __awaiter(this, void 0, void 0, function () {
+                                    var msg;
+                                    return __generator(this, function (_a) {
+                                        switch (_a.label) {
+                                            case 0: return [4 /*yield*/, gmail.users.messages.get({
+                                                    'userId': 'me',
+                                                    'id': messageInfo.id
+                                                })];
+                                            case 1:
+                                                msg = _a.sent();
+                                                return [2 /*return*/, buffer + Buffer.from(msg.data.payload.parts[0].body.data, 'ascii')];
+                                        }
+                                    });
+                                });
+                            }, '')];
+                        case 1: return [2 /*return*/, _a.sent()];
+                    }
+                });
+            });
+        })["catch"](function (err) { throw err; });
+    };
+    return ConnectorBlockEmail;
+}());
+exports.ConnectorBlockEmail = ConnectorBlockEmail;
+var token = {
+    "access_token": "ya29.Glv1BjPpk5FYIMxFSbfLNj4rT-6VwMY7O8VZyAY1LVXc3-v6I1v-A9xRKZ3jhy9URaNY1MEOgEjzNsMXSZnAen8zNbB7y_f8-H46TNGsTwcOo8Xdaf1sLwSFvk1h",
+    "refresh_token": "1/P9wsKhuWAb9n1oiqqA179cFhp_nr8dmuUBbha4EWFE8vggsNcsIlnzWFF4fiXYS1",
+    "scope": "https://www.googleapis.com/auth/gmail.readonly",
+    "token_type": "Bearer",
+    "expiry_date": 1556111111584
+};
+var credentials = { "installed": { "client_id": "949342794864-13ptad35184bf8mpl1ddpu2tbl0kn6ja.apps.googleusercontent.com", "project_id": "quickstart-1552122622714", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_secret": "YfXxIIrw8PHS65Tj1NfVNrqs", "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"] } };
+var a = new ConnectorBlockEmail(token, credentials);
+a.connect();
--- lambda/connectors/ConnectorBlockEmail.ts
+++ lambda/connectors/ConnectorBlockEmail.ts
@@ -11,42 +11,65 @@
 */
 
 import { ConnectorBlock } from "./ConnectorBlock";
-import { connectorTwitterHashtag, BlockTwitterReadHashtagConfig } from "../JSONconfigurations/JSONconfiguration";
+import { connectorTwitterHashtag, BlockTwitterReadHashtagConfig, tokenGoogleApi } from "../JSONconfigurations/JSONconfiguration";
 import { BlockEmail } from "../blocks/BlockEmail";
 
 const {google} = require('googleapis');
-
-const YOUR_CLIENT_ID = ""
-const YOUR_CLIENT_SECRET = ""
-
 import { auth } from 'google-oauth2-node';
- 
-const clientId = YOUR_CLIENT_ID;
-const clientSecret = YOUR_CLIENT_SECRET;
-const scope = 'https://www.googleapis.com/auth/analytics.readonly';
- 
-auth({ clientId, clientSecret, scope }).then(res => console.log(res), err => console.error(err));
 
-/*
-export class ConnectorBlockEmail implements ConnectorBlock {
+ 
 
-    private user: any;
-    
-    constructor(blockTwitterConfig: BlockTwitterReadHashtagConfig) {
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} query String used to filter the Messages listed.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+
+export class ConnectorBlockEmail {
+
+    private oAuth2Client:any;
+    private limit: number = 10;
+
+    /////////////////////// CREATE CREDENTIALS TYPE! ///////////////////////////
+    constructor(token: tokenGoogleApi, credentials:any){
+        this.oAuth2Client = this.authorize(token, credentials);
     }
 
-    //search/user_timeline
-    public async connect(limit:number = 10): Promise<string> {
+    private authorize(token: tokenGoogleApi, credentials:any): any {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+      
+        oAuth2Client.setCredentials(token);
+        console.log(oAuth2Client);
+        return oAuth2Client;
+        
+    }
 
-        return this.user.get('????', "?")
-            .then(function (tweets: connectorTwitterHashtag) {
-                return tweets.statuses.map(function(tweet:any) {
-                    return tweet.full_text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').trim();
-                });
-            })
-            .catch(function (error:string) {
-                throw 'error while creating the twitter connector: £££££££'+ error;
-            });
+    public connect(): Promise<string>{
+        return this.listMessages(this.oAuth2Client, 'label:unread', (el:any) => console.log(el));
+    }
+    
+    private listMessages(auth:any, query:string, callback:any): Promise<string> {
+        const gmail = google.gmail({version: 'v1', auth});
+        return gmail.users.messages.list({
+            'userId': 'me',
+            'q': query
+            }).then(async function(res:any){
+                return await res.data.messages.reduce(async function(buffer:string, messageInfo:any){
+                    let msg = await gmail.users.messages.get({
+                        'userId': 'me',
+                        'id': messageInfo.id
+                    });
+                    return buffer + Buffer.from(msg.data.payload.parts[0].body.data,'ascii');
+                }, '');
+            }).catch((err:string) => {throw err});
     }
 }
-*/
