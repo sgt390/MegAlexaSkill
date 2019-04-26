56499ffeb9c9775c6153be20dec19d31a407737d
Matteo <depascale.matteo@gmail.com>
2019-04-26 20:34:30 +0200

    first intestation

--- lambda/blocks/utility/ElicitBlock.ts
+++ lambda/blocks/utility/ElicitBlock.ts
@@ -1,13 +1,16 @@
 /*
 * File: ElicitBlock.ts
-* Version: 0.0.1
+* Version: 1.0.0
 * Date: 2019-03-26
 * Author: Bianca Andreea Ciuche
 * License:
 *
 * History:
-* Author                || Date         || Description
+* Author                    || Date         || Description
 * Bianca Andreea Ciuche     || 2019-03-26   || Created file
+* Matteo Depascale          || 2019-03-27   || Implemented clasd
+* Stefano Zanatta           || 2019-03-28   || Verified
+* Matteo Depascale          || 2019-04-10   || Approved
 */
 import {Block} from "../Block";
 
